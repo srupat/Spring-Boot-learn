caffe9cd9d11da15d7b7b0fe5e3e0b4fc8ccbfd8
srupat <srujanpatwardhan@gmail.com>
2024-01-16 07:54:41 +0530

    chore : file structure improved

--- demo/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ demo/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo.securingweb;
+package com.example.securingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
